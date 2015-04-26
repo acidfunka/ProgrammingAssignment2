8034b2b4b7936d64f49ec105bbf9ccff350e4640
acidfunka <acidfunka1@gmail.com>
2015-04-26 13:38:13 +0200

    commit for assignment 2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Introduction 1
+### Introduction
 
 This second programming assignment will require you to write an R
 function that is able to cache potentially time-consuming computations.
--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates the cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        setmatrix<-function(solve) m<<- solve
+        getmatrix<-function() m
+        list(set=set, get=get,
+             setmatrix=setmatrix,
+             getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function here
+## This function determines if the matrix is cached and then used it to invert the matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+        m<-x$getmatrix()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        matrix <- x$get()
+        m<-solve(matrix, ...)
+        x$setmatrix(m)
+        m
 }
